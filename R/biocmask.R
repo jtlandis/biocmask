cba1e37f410c839b4510b636d0514ef7217f7428
jtlandis <jtlandis314@gmail.com>
2024-08-01 16:48:35 -0400

    applying details argument

--- R/biocmask.R
+++ R/biocmask.R
@@ -13,10 +13,17 @@ new_biocmask.SummarizedExperiment <- function(obj, ...) {
   groups <- metadata(obj)[["group_data"]]
   details <- group_details(obj)
   shared_ctx_env <- prepare_shared_ctx_env(details)
-  mask_assay <- biocmask_assay$new(assays(obj), get_group_indices(groups, "assay"), .env = shared_ctx_env,
-                                   .nrow = nrow(obj), .ncol = ncol(obj))
-  mask_rows <- biocmask$new(rowData(obj), get_group_indices(groups, "rowData"), .env = shared_ctx_env)
-  mask_cols <- biocmask$new(colData(obj), get_group_indices(groups, "colData"), .env = shared_ctx_env)
+  mask_assay <- biocmask_assay$new(assays(obj),
+                                   get_group_indices(groups, details, "assay"),
+                                   .env = shared_ctx_env,
+                                   .nrow = nrow(obj),
+                                   .ncol = ncol(obj))
+  mask_rows <- biocmask$new(rowData(obj),
+                            get_group_indices(groups, details, "rowData"),
+                            .env = shared_ctx_env)
+  mask_cols <- biocmask$new(colData(obj),
+                            get_group_indices(groups, details, "colData"),
+                            .env = shared_ctx_env)
   
   extended_environments <- connect_masks(mask_assays = mask_assay,
                                          mask_rows = mask_rows,
