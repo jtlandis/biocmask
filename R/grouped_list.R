997fe1b25c93255eb043eeaab5799a5053a75175
jtlandis <jtlandis314@gmail.com>
2024-08-02 11:03:03 -0400

    Still want to consider case_key as a solution to multigroups. but for now we will leave it be.

--- R/grouped_list.R
+++ R/grouped_list.R
@@ -1,19 +1,19 @@
 
-library(vctrs)
-
-
-new_grouped_lst <- function(x, keys) {
-  x1 <- list_of(!!!x[[1]])
-
-  vctrs::new_list_of(x, ptype = vec_ptype(x1), class = "vctrs_grouped_list", .key = keys)
-}
-# .out <- vec_chop(1:10, list(1:2,3:5, 6:9, 10)) |> list() |> new_grouped_lst(keys = tibble(condition = c("cntrl","drug")))
-
-vec_ptype_abbr.vctrs_grouped_list <- function(x, ...) {
-  paste0("grpd<",
-         vec_ptype_abbr(attr(attr(x, "ptype"), "ptype"))
-         ,"[", nrow(attr(x, ".key")),"]>")
-}
+# library(vctrs)
+# 
+# 
+# new_grouped_lst <- function(x, keys) {
+#   x1 <- list_of(!!!x[[1]])
+# 
+#   vctrs::new_list_of(x, ptype = vec_ptype(x1), class = "vctrs_grouped_list", .key = keys)
+# }
+# # .out <- vec_chop(1:10, list(1:2,3:5, 6:9, 10)) |> list() |> new_grouped_lst(keys = tibble(condition = c("cntrl","drug")))
+# 
+# vec_ptype_abbr.vctrs_grouped_list <- function(x, ...) {
+#   paste0("grpd<",
+#          vec_ptype_abbr(attr(attr(x, "ptype"), "ptype"))
+#          ,"[", nrow(attr(x, ".key")),"]>")
+# }
 
 case_key <- function(x, ...) {
   if (!inherits(x, "vctrs_grouped_list")) {
@@ -56,33 +56,33 @@ case_key <- function(x, ...) {
 }
 
 
-pillar_shaft.vctrs_grouped_list <- function(x, ...) {
-  out <- map_chr(
-    x,
-    ~ sprintf("[%s]",
-              paste0(pillar::style_subtle(map_chr(.x, size_sum)),
-                     collapse = ","))
-  )
-  pillar::new_pillar_shaft_simple(out, align = "left", min_width = 10, shorten = "back")
-}
-
-# tibble(y = 1, z = 10, a = "A", x = .out)
-
-# rowd <- rowData(se) |> as_tibble(rownames = ".features")
-# col_data_chop <- colData(se) |> as_tibble(rownames = ".samples") |> tidyr::chop(-condition)
-# rowd$samples <- list()
-#
-# setClass("vctrs_grouped_list", contains = "list")
-#
-# setMethod(lapply, signature(X = "vctrs_grouped_list"),
-#           function(X, FUN, ...) {
-#             base::lapply(
-#               X,
-#               function(x, ...) {
-#                 base::lapply(X = x, FUN = FUN, ...)
-#               },
-#               ...
-#             ) |>
-#               new_list_of(ptype = vctrs::vec_ptype(X[[1]]), class = "vctrs_grouped_list", .key = attr(X, ".key"))
-#           })
-
+# pillar_shaft.vctrs_grouped_list <- function(x, ...) {
+#   out <- map_chr(
+#     x,
+#     ~ sprintf("[%s]",
+#               paste0(pillar::style_subtle(map_chr(.x, size_sum)),
+#                      collapse = ","))
+#   )
+#   pillar::new_pillar_shaft_simple(out, align = "left", min_width = 10, shorten = "back")
+# }
+# 
+# # tibble(y = 1, z = 10, a = "A", x = .out)
+# 
+# # rowd <- rowData(se) |> as_tibble(rownames = ".features")
+# # col_data_chop <- colData(se) |> as_tibble(rownames = ".samples") |> tidyr::chop(-condition)
+# # rowd$samples <- list()
+# #
+# # setClass("vctrs_grouped_list", contains = "list")
+# #
+# # setMethod(lapply, signature(X = "vctrs_grouped_list"),
+# #           function(X, FUN, ...) {
+# #             base::lapply(
+# #               X,
+# #               function(x, ...) {
+# #                 base::lapply(X = x, FUN = FUN, ...)
+# #               },
+# #               ...
+# #             ) |>
+# #               new_list_of(ptype = vctrs::vec_ptype(X[[1]]), class = "vctrs_grouped_list", .key = attr(X, ".key"))
+# #           })
+# 
