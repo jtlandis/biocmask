3ef3a2947eaf65f00393ba2f47deb360094c0bb2
jtlandis <jtlandis314@gmail.com>
2024-08-07 21:56:52 -0400

    new exports

---
+++ R/reexports-SummarizedExperiment.R
@@ -0,0 +1,33 @@
+
+#'@export
+SummarizedExperiment::SummarizedExperiment
+
+#' @export
+SummarizedExperiment::assay
+
+#' @export
+SummarizedExperiment::`assay<-`
+
+#' @export
+SummarizedExperiment::assays
+
+#' @export
+SummarizedExperiment::`assays<-`
+
+#' @export
+SummarizedExperiment::rowData
+
+#' @export
+SummarizedExperiment::`rowData<-`
+
+#' @export
+SummarizedExperiment::colData
+
+#' @export
+SummarizedExperiment::`colData<-`
+
+#' @export
+S4Vectors::metadata
+
+#' @export
+S4Vectors::`metadata<-`
