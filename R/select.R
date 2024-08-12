27eddde294e716e464b4f7f7d159b9c64a890ab9
jtlandis <jtlandis314@gmail.com>
2024-08-11 22:09:28 -0400

    This a very simple `select()` function. I have not yet considered how to "rename" a value.

---
+++ R/select.R
@@ -0,0 +1,50 @@
+
+
+
+select.SummarizedExperiment <- function(.data, ...) {
+  
+  .env <- caller_env()
+  poke_ctx_local("biocmask:::caller_env", .env)
+  poke_ctx_local("biocmask:::dplyr_verb", "select")
+  quos <- biocmask_quos(...)
+  ctxs <- vapply(quos, attr, FUN.VALUE = "", which = "biocmask:::ctx")
+  nms  <- names(quos)
+  
+  selected <- biocmask_eval_select(
+    quos = quos, ctxs = ctxs, data = .data
+  )
+  
+  
+  assays(.data) <- assays(.data)[selected[["assays"]]]
+  rowData(.data) <- rowData(.data)[selected[["rows"]]]
+  colData(.data) <- colData(.data)[selected[["cols"]]]
+  
+  .data
+  
+}
+
+
+biocmask_eval_select <- function(quos, ctxs, data) {
+  
+  out <- vector("list", length(quos))
+  .data <- list(
+    assays = as.list(assays(data)),
+    rows = as.list(rowData(data)),
+    cols = as.list(colData(data))
+  )
+  
+  for (i in seq_along(quos)) {
+    quo <- quos[[i]]
+    ctx <- ctxs[i]
+    out[[i]] <- tidyselect::eval_select(quo, .data[[ctx]])
+  }
+  
+  ctxs <- factor(ctxs, levels = c("assays","rows","cols"))
+  split(out, ctxs) |>
+    map(function(.x) {
+      unlist(.x) |>
+        unique()
+    })
+  
+}
+
