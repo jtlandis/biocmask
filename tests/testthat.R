bc3d35e46fe532e4dd5c0586b20c85cc2cb4fa80
Mike Love <mikelove@users.noreply.github.com>
2024-09-11 10:29:25 -0400

    adding some biolerplates

---
+++ tests/testthat.R
@@ -0,0 +1,3 @@
+library("testthat")
+library("biocmask")
+test_check("biocmask")
---
+++ tests/testthat/test_basics.R
@@ -0,0 +1,5 @@
+context("basic testing")
+test_that("mutate works as expected", {
+  # don't need to load package
+  expect_true(TRUE)
+})
