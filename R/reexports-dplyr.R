0cf0790c1ba63c6232cbcd318ad1ae6c142d0b3d
jtlandis <jtlandis314@gmail.com>
2024-09-13 17:01:41 -0400

    Re-exports

--- R/reexports-dplyr.R
+++ R/reexports-dplyr.R
@@ -2,29 +2,29 @@
 # The following functions should
 # be available to users from our package
 
-# #' @export
-# dplyr::arrange
-# 
-# #' @export
-# dplyr::mutate
-# 
-# #' @export
-# dplyr::filter
-# 
-# #' @export
-# dplyr::summarise
-# 
-# #' @export
-# dplyr::select
-# 
-# #' @export
-# dplyr::group_by
-# 
-# #' @export
-# dplyr::group_data
-# 
-# #' @export
-# dplyr::group_vars
-# 
-# #' @export
-# dplyr::ungroup
+#' @export
+dplyr::arrange
+
+#' @export
+dplyr::mutate
+
+#' @export
+dplyr::filter
+
+#' @export
+dplyr::summarise
+
+#' @export
+dplyr::select
+
+#' @export
+dplyr::group_by
+
+#' @export
+dplyr::group_data
+
+#' @export
+dplyr::group_vars
+
+#' @export
+dplyr::ungroup
