4eb59826f3f0dcba023e99c8110291b5174af1cd
jtlandis <jtlandis314@gmail.com>
2024-10-09 13:29:53 -0400

    Checking loading of conflicting packages

---
+++ R/zzz.R
@@ -0,0 +1,89 @@
+
+conflict_info <- paste0(
+    "Note that these packages are exclusive and present different APIs ",
+    "for working with `SummarizedExperiment` objects. As such conflicts ",
+    "and unexpected behaviors may arise!"
+)
+
+rule <- function (pad = "-", gap = 2L) {
+  paste0(rep(pad, getOption("width") - gap), collapse = "")
+}
+
+attaching_tidySE <- quote(library(tidySummarizedExperiment))
+
+# a package can either be loaded, or completely attached. R only informs
+# of conflicts when the package is loaded, and when attached these can be
+# overwritten silently
+
+.onAttach <- function(libname, pkgname) {
+  # check if `tidySummarizedExperiment` is already loaded
+  conflict <- grep("tidySummarizedExperiment", search(), value = TRUE)
+  if (length(conflict)) {
+    packageStartupMessage(rule())
+    packageStartupMessage(
+        "you have loaded `biocmask` after `tidySummarizedExperiment`\n",
+        conflict_info
+        )
+    packageStartupMessage(rule())
+  }
+  # set hook for if `tidySummarizedExperiment` gets attached latter
+  setHook(
+    packageEvent(pkgname = "tidySummarizedExperiment",
+                 event = "attach"),
+    function(...) {
+      packageStartupMessage(rule())
+      packageStartupMessage(
+        "You have loaded `tidySummarizedExperiment` after `biocmask`\n",
+        conflict_info
+      )
+      packageStartupMessage(rule())
+    }
+    
+  )
+  # hook for when tidySummarizedExperiment:: is used after biocmask is attached
+  setHook(
+    packageEvent(pkgname = "tidySummarizedExperiment",
+                 event = "onLoad"),
+    function(...) {
+      calls <- sys.calls()
+      called_via_library <- vapply(calls, identical, y = attaching_tidySE,
+                                   logical(1))
+      if (!any(called_via_library)) {
+        # if this callback was not executed via library(tidySummarizedExperiment)
+        # then warn that biocmask dplyr methods have likely been overwritten!
+        # otherwise, let the .onAttach method warn the user
+        rlang::inform(
+          .frequency = "regularly", 
+          .frequency_id = "tidySummarizedExperiment_loaded_after_biocmask_attach",
+          class = "biocmask_loaded_conflict",
+          message = c(
+            paste0(
+              "You have likely loaded `tidySummarizedExperiment` after ",
+              "`biocmask` via `tidySummarizedExperiment::...`"
+            ),
+            "This has likely caused `dplyr` methods from `biocmask` to be over-written!",
+            conflict_info))
+      }
+      
+    }
+  )
+}
+
+.onDetach <- function(libpath) {
+  # remove verbose message when `biocmask` is removed from attach list
+  setHook(packageEvent("tidySummarizedExperiment", "attach"), NULL, "replace")
+  setHook(packageEvent("tidySummarizedExperiment", "onLoad"), NULL, "replace")
+}
+
+# check_dplyr_s3_method <- function(method) {
+#   tbl <- getNamespace("dplyr")[[".__S3MethodsTable__."]]
+#   res <- tbl[[method]]
+#   if (!is.null(res) && is.function(res)) {
+#     env_name <- environmentName(environment(res))
+#     if (env_name != "biocmask") {
+#       rlang::warn(
+#         paste0(c("`biocmask` shows that `", env_name, "` is already loaded")))
+#     }
+#   }
+#   res
+# }
