0ffe4109a0353ed28f7605e56b0128d1d2392551
jtlandis <jtlandis314@gmail.com>
2025-10-14 14:22:23 -0400

    moving to S7

--- R/bioc-cast.r
+++ R/bioc-cast.r
@@ -2,45 +2,43 @@
 #' @include bioc-ptype2.R
 #' @include bioc-init.r
 
-setGeneric(
+bioc_cast <- S7::new_generic(
   "bioc_cast",
-  signature = c("from", "to"),
+  dispatch_args = c("from", "to"),
   function(from, to, ...) {
-    standardGeneric("bioc_cast")
+    S7_dispatch()
   }
 )
 
-setMethod(
-  "bioc_cast",
+S7::method(
+  bioc_cast,
   signature = list(
-    from = methods::getClass("ANY"),
-    to = methods::getClass("NULL")
-  ),
-  def = function(from,
-                 to,
-                 ...) {
-    NULL
-  }
-)
+    from = S7::class_any,
+    to = NULL
+  )
+) <- function(from,
+              to,
+              ...) {
+  NULL
+}
 
-setMethod(
-  "bioc_cast",
+S7::method(
+  bioc_cast,
   signature = list(
-    from = methods::getClass("NULL"),
-    to = methods::getClass("ANY")
-  ),
-  def = function(from,
-                 to,
-                 ...) {
-    bioc_ptype(to, ...)
-  }
-)
+    from = NULL,
+    to = S7::class_any
+  )
+) <- function(from,
+              to,
+              ...) {
+  bioc_ptype(to, ...)
+}
 
-setMethod(
-  "bioc_cast",
+S7::method(
+  bioc_cast,
   signature = list(
-    from = methods::getClass("ANY"),
-    to = methods::getClass("ANY")
+    from = S7::class_any,
+    to = S7::class_any
   ),
   def = function(from,
                  to,
@@ -55,216 +53,157 @@ setMethod(
   }
 )
 
-setMethod(
-  "bioc_cast",
-  signature = list(from = class_vec, to = class_vec),
-  def = function(from,
-                 to,
-                 ...) {
-    vctrs::vec_cast(
-      x = from,
-      to = to,
-      ...,
-      x_arg = "from",
-      y_arg = "to",
-      call = caller_env()
-    )
-  }
-)
+S7::method(
+  bioc_cast,
+  signature = list(from = class_vec, to = class_vec)
+) <- function(from,
+              to,
+              ...) {
+  vctrs::vec_cast(
+    x = from,
+    to = to,
+    ...,
+    x_arg = "from",
+    y_arg = "to",
+    call = caller_env()
+  )
+}
 
-setMethod(
-  "bioc_cast",
-  signature = list(from = class_vec, to = class_vctrs_vec),
-  def = function(from,
-                 to,
-                 ...) {
-    vctrs::vec_cast(
-      x = from,
-      to = to,
-      ...,
-      x_arg = "from",
-      y_arg = "to",
-      call = caller_env()
-    )
-  }
-)
+S7::method(
+  bioc_cast,
+  signature = list(from = class_vec, to = class_vctrs_vec)
+) <- function(from,
+              to,
+              ...) {
+  vctrs::vec_cast(
+    x = from,
+    to = to,
+    ...,
+    x_arg = "from",
+    y_arg = "to",
+    call = caller_env()
+  )
+}
 
 
-setMethod(
-  "bioc_cast",
-  signature = list(from = class_vctrs_vec, to = class_vec),
-  def = function(from,
-                 to,
-                 ...) {
-    vctrs::vec_cast(
-      x = from,
-      to = to,
-      ...,
-      x_arg = "from",
-      y_arg = "to",
-      call = caller_env()
-    )
-  }
-)
+S7::method(
+  bioc_cast,
+  signature = list(from = class_vctrs_vec, to = class_vec)
+) <- function(from,
+              to,
+              ...) {
+  vctrs::vec_cast(
+    x = from,
+    to = to,
+    ...,
+    x_arg = "from",
+    y_arg = "to",
+    call = caller_env()
+  )
+}
 
-setMethod(
-  "bioc_cast",
-  signature = list(from = class_vctrs_vec, to = class_vctrs_vec),
-  def = function(from,
-                 to,
-                 ...) {
-    vctrs::vec_cast(
-      x = from,
-      to = to,
-      ...,
-      x_arg = "from",
-      y_arg = "to",
-      call = caller_env()
-    )
-  }
-)
+S7::method(
+  bioc_cast,
+  signature = list(from = class_vctrs_vec, to = class_vctrs_vec)
+) <- function(from,
+              to,
+              ...) {
+  vctrs::vec_cast(
+    x = from,
+    to = to,
+    ...,
+    x_arg = "from",
+    y_arg = "to",
+    call = caller_env()
+  )
+}
 
-setMethod(
-  "bioc_cast",
-  signature = list(from = class_df, to = class_df),
-  def = function(from,
-                 to,
-                 ...) {
-    vctrs::vec_cast(
-      x = from,
-      to = to,
-      ...,
-      x_arg = "from",
-      y_arg = "to",
-      call = caller_env()
-    )
-  }
-)
+S7::method(
+  bioc_cast,
+  signature = list(from = class_df, to = class_df)
+) <- function(from,
+              to,
+              ...) {
+  vctrs::vec_cast(
+    x = from,
+    to = to,
+    ...,
+    x_arg = "from",
+    y_arg = "to",
+    call = caller_env()
+  )
+}
 
-setMethod(
-  "bioc_cast",
-  signature = list(from = class_DF, to = class_df),
-  def = function(from,
-                 to,
-                 ...) {
-    to <- vctrs::vec_ptype(to, x_arg = "y", call = caller_env()) |>
-      as.list()
-    xnames <- names(from)
-    ynames <- names(to)
-    if (!all(xnames %in% ynames)) {
-      rlang::abort(
-        sprintf(
-          "cannot cast a DataFrame with columns (%s) to a data.frame with columns (%s)",
-          paste(xnames, collapse = ", "),
-          paste(ynames, collapse = ", ")
-        )
+bioc_cast_DF <- function(from,
+                         to,
+                         ...) {
+  to <- bioc_ptype(to, x_arg = "y", call = caller_env()) |>
+    as.list()
+  xnames <- names(from)
+  ynames <- names(to)
+  if (!all(xnames %in% ynames)) {
+    rlang::abort(
+      sprintf(
+        "cannot cast a DataFrame with columns (%s) to a data.frame with columns (%s)",
+        paste(xnames, collapse = ", "),
+        paste(ynames, collapse = ", ")
       )
-    }
-    for (name in xnames) {
-      to[[name]] <- bioc_cast(from[[name]], to[[name]])
-    }
-    ynames <- setdiff(ynames, xnames)
-    size <- bioc_size(from)
-    for (name in ynames) {
-      to[[name]] <- bioc_init(to[[name]], size = size)
-    }
-    new_DF(to, nrows = size, rownames = rownames(from))
+    )
   }
-)
+  for (name in xnames) {
+    to[[name]] <- bioc_cast(from[[name]], to[[name]])
+  }
+  ynames <- setdiff(ynames, xnames)
+  size <- bioc_size(from)
+  for (name in ynames) {
+    to[[name]] <- bioc_init(to[[name]], size = size)
+  }
+  new_DF(to, nrows = size, rownames = rownames(from))
+}
 
-setMethod(
-  "bioc_cast",
-  signature = list(from = class_df, to = class_DF),
-  def = function(from,
-                 to,
-                 ...) {
-    to <- vctrs::vec_ptype(to, x_arg = "y", call = caller_env()) |>
-      as.list()
-    xnames <- names(from)
-    ynames <- names(to)
-    if (!all(xnames %in% ynames)) {
-      rlang::abort(
-        sprintf(
-          "cannot cast a DataFrame with columns (%s) to a data.frame with columns (%s)",
-          paste(xnames, collapse = ", "),
-          paste(ynames, collapse = ", ")
-        )
-      )
-    }
-    for (name in xnames) {
-      to[[name]] <- bioc_cast(from[[name]], to[[name]])
-    }
-    ynames <- setdiff(ynames, xnames)
-    size <- bioc_size(from)
-    for (name in ynames) {
-      to[[name]] <- bioc_init(to[[name]], size = size)
-    }
-    new_DF(to, nrows = size, rownames = rownames(from))
-  }
-)
+S7::method(
+  bioc_cast,
+  signature = list(from = class_DF, to = class_df)
+) <- bioc_cast_DF
 
-setMethod(
-  "bioc_cast",
-  signature = list(from = class_DF, to = class_DF),
-  def = function(from,
-                 to,
-                 ...) {
-    to <- vctrs::vec_ptype(to, x_arg = "y", call = caller_env()) |>
-      as.list()
-    xnames <- names(from)
-    ynames <- names(to)
-    if (!all(xnames %in% ynames)) {
-      rlang::abort(
-        sprintf(
-          "cannot cast a DataFrame with columns (%s) to a data.frame with columns (%s)",
-          paste(xnames, collapse = ", "),
-          paste(ynames, collapse = ", ")
-        )
-      )
-    }
-    for (name in xnames) {
-      to[[name]] <- bioc_cast(from[[name]], to[[name]])
-    }
-    ynames <- setdiff(ynames, xnames)
-    size <- bioc_size(from)
-    for (name in ynames) {
-      to[[name]] <- bioc_init(to[[name]], size = size)
-    }
-    new_DF(to, nrows = size, rownames = rownames(from))
-  }
-)
+S7::method(
+  bioc_cast,
+  signature = list(from = class_df, to = class_DF)
+) <- bioc_cast_DF
 
-setMethod(
-  "bioc_cast",
-  signature = list(from = class_s4_vec, y = class_s4_vec),
-  def = function(from, to, ...) {
-    methods::as(from, class(to))
-  }
-)
+S7::method(
+  bioc_cast,
+  signature = list(from = class_DF, to = class_DF)
+) <- bioc_cast_DF
 
-setMethod(
-  "bioc_cast",
-  signature = list(from = class_vec, y = class_s4_vec),
-  def = function(from, to, ...) {
-    methods::as(from, class(to))
-  }
-)
+S7::method(
+  bioc_cast,
+  signature = list(from = class_s4_vec, y = class_s4_vec)
+) <- function(from, to, ...) {
+  methods::as(from, class(to))
+}
 
-setMethod(
-  "bioc_cast",
-  signature = list(from = class_s4_vec, y = class_vec),
-  def = function(from, to, ...) {
-    methods::as(from, class(to))
-  }
-)
+S7::method(
+  bioc_cast,
+  signature = list(from = class_vec, y = class_s4_vec)
+) <- function(from, to, ...) {
+  methods::as(from, class(to))
+}
+
+S7::method(
+  bioc_cast,
+  signature = list(from = class_s4_vec, y = class_vec)
+) <- function(from, to, ...) {
+  methods::as(from, class(to))
+}
 
 # attempt_ptype2 <- function(x, y) {
 #   out <- c(x, y)
 #   out[0]
 # }
 
-# setMethod(
-#   "bioc_ptype2",
+# S7::method(
+#   bioc_ptype2,
 #   signature = list(x = class_s4_vec, y = class_s4_vec),
 #   def = function(x,
 #                  y,
@@ -273,8 +212,8 @@ setMethod(
 #   }
 # )
 
-# setMethod(
-#   "bioc_ptype2",
+# S7::method(
+#   bioc_ptype2,
 #   signature = list(x = class_vec, y = class_s4_vec),
 #   def = function(x,
 #                  y,
@@ -283,8 +222,8 @@ setMethod(
 #   }
 # )
 
-# setMethod(
-#   "bioc_ptype2",
+# S7::method(
+#   bioc_ptype2,
 #   signature = list(x = class_s4_vec, y = class_vec),
 #   def = function(x,
 #                  y,
